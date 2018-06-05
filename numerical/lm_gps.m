273b558b6988f4326776d8a0619dd74f19a0ac73
bvarga92 <bvarga92@gmail.com>
2018-06-05 21:07:59 +0200

    update

--- numerical/lm_gps.m
+++ numerical/lm_gps.m
@@ -3,7 +3,7 @@ clc;
 
 %% parameterek
 use_fsolve=false;       % fsolve-val vagy sajat eljarassal oldjuk meg?
-sats=[                  % muholdak pozicioja
+sats=[                  % muholdak pozicioja (x y z [km])
    15600   7540  20140;
    18760   2750  18610;
    17610  14630  13480;
@@ -11,7 +11,7 @@ sats=[                  % muholdak pozicioja
 %    17800   6400  18660;
 %    17500   7590  18490
 ];
-t=[                     % kesleltetesek
+t=[                     % kesleltetesek [s]
    0.07074;
    0.07220;
    0.07690;
@@ -19,8 +19,8 @@ t=[                     % kesleltetesek
 %    0.07320;
 %    0.07350
 ];
-Re=6371;                % a Fold sugara
-c=3e5;                  % fenysebesseg
+Re=6371;                % a Fold sugara [km]
+c=3e5;                  % fenysebesseg [s]
 res=@(xyzd) sqrt((sats(:,1)-xyzd(1)).^2+(sats(:,2)-xyzd(2)).^2+(sats(:,3)-xyzd(3)).^2)-c*(t-xyzd(4));
 
 %% megoldas
@@ -45,7 +45,7 @@ else
     lambda_c=0.75;
     d=xTol;
     ii=1;
-    while ii<=maxIter && any(abs(d)>=xTol) && any(abs(r)>=fTol(:))
+    while ii<=maxIter && any(abs(d)>=xTol) && any(abs(r)>=fTol)
         d=pinv(A+lambda*eye(length(x)))*v;
         xd=x-d;
         rd=res(xd);
